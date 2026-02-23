0b6c129d6250e820d2fc327f1595b2574a646b8d
Abhishek Verma <neeleshabhishek@gmail.com>
2026-02-24 00:21:54 +0530

    leave schema done

---
+++ server/models/Leave.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose")
+
+const leaveSchema =new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required:true,
+    },
+    fromDate: {
+        type: Date,
+        required:true,
+    },
+    toDate: {
+        type: Date,
+        required:true,
+    },
+    reason: {
+        type: String,
+        required: true,
+        trim :true,
+    },
+    status: {
+        type: String,
+        enum: ["pending", "approved", "rejected"],
+        default:"pending"
+    }
+},
+    {
+        timestamps:true,
+    }
+)
+
+module.exports=mongoose.model("Leave",leaveSchema)
