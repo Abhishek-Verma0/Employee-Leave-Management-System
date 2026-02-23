dbf8dfc44b7ce030fc98d4ed1789c0b19d258bca
Abhishek Verma <neeleshabhishek@gmail.com>
2026-02-23 23:49:44 +0530

    db connection and userSchema done

---
+++ server/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+
+const connectDb = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_DB_URL);
+        console.log("mongo connected")
+        
+    } catch (err) {
+        console.log("error while connecting db")
+    }
+}
+
+module.exports=connectDb
--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
 const express = require("express")
+const connectDb = require("./config/db");
+const dotenv=require("dotenv")
+
+
+dotenv.config() // laoding env
+//  conncecting db
+
+connectDb()
 
 const app = express()
-
+app.use(express.json());
 const port = 3000
 
 
 
 
-
 app.get("/", (req,res) => {
-    res.send("Hola ")
+    res.send("Hola")
 })
 
 
