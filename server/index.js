5b745cd82e7a71306a2718c735dbc3ba873413e7
Abhishek Verma <neeleshabhishek@gmail.com>
2026-02-26 04:06:27 +0530

    config for deployment

--- server/index.js
+++ server/index.js
@@ -27,10 +27,12 @@ if (process.env.NODE_ENV === "production") {
     const clientPath = path.join(__dirname, "../client/dist");
     app.use(express.static(clientPath));
 
-    app.get("/*", (req, res) => {
+    // React SPA fallback (Express 5 compatible)
+    app.get("/:path(*)", (req, res) => {
         res.sendFile(path.join(clientPath, "index.html"));
     });
-} else {
+}
+else {
     app.get("/", (req, res) => {
         res.send("Swiftly Server is running in development mode (API only)");
     });
