59c576b1fb79dbb6a67ad49fa3b0a6bd13201bba
Abhishek Verma <144248104+Abhishek-Verma0@users.noreply.github.com>
2026-06-03 15:20:37 +0530

    Update reimbursemnetRoute.js

--- server/routes/reimbursemnetRoute.js
+++ server/routes/reimbursemnetRoute.js
@@ -24,7 +24,7 @@ const upload = multer({
             cb(error);
          }
         }
-    }
+    
 });
 
 const { applyReimbursement, getReimbursement, getAllReimbursement, updateReimbursement, updateBill, deleteBill } = require("../controllers/reimbursementController")
