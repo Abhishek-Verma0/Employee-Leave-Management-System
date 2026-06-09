c72f1d94ee2507184c6cd2b377db603c109b93a6
Rakshita Kumari <rakshita0903@gmail.com>
2026-06-09 22:36:15 +0530

    fix: use Zod v4 error option instead of errorMap for enum validation

--- server/validators/user.validator.js
+++ server/validators/user.validator.js
@@ -2,9 +2,7 @@ const { z } = require("zod");
 
 const updateUserRoleSchema = z.object({
     role: z.enum(["employee", "manager", "admin"], {
-        errorMap: () => ({
-            message: "Role must be one of: employee, manager, admin",
-        }),
+        error: "Role must be one of: employee, manager, admin",
     }),
 });
 
