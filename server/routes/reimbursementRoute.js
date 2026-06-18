27f76bec642478debec550df6594643224bb4e55
Abhishek Verma <144248104+Abhishek-Verma0@users.noreply.github.com>
2026-06-18 22:51:32 +0530

    Update reimbursementRoute.js

--- server/routes/reimbursementRoute.js
+++ server/routes/reimbursementRoute.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const multer = require("multer")
 
-const upload = multer({ storage: multer.memoryStorage(), limits: { fileSize: 5 * 1024 * 1024 } }) // 5MB limit
+
 const upload = multer({
     storage: multer.memoryStorage(),
 
